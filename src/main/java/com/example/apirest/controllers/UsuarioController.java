44f5021d8dd664672d0b902b57d06ba632af9a76
Jarlin Andres Fonseca Bermon <jarlinandres5000@gmail.com>
2023-02-01 20:31:09 -0500

    Api Rest de CRUD Springboot

---
+++ src/main/java/com/example/apirest/controllers/UsuarioController.java
@@ -0,0 +1,43 @@
+package com.example.apirest.controllers;
+
+import com.example.apirest.models.UsuarioModel;
+import com.example.apirest.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+
+    @Autowired
+    UsuarioService usuarioService;
+
+    @GetMapping()
+    public ArrayList<UsuarioModel> obtenerUsuarios(){
+        return usuarioService.obtenerUsuarios();
+    }
+
+    @PostMapping()
+    public UsuarioModel guardarUsuario(@RequestBody UsuarioModel usuario){
+        return this.usuarioService.guardarUsuario(usuario);
+    }
+
+    @GetMapping(path = "/{id}")
+    public Optional<UsuarioModel> obtenerUsuarioPorId(@PathVariable("id") Long id){
+        return this.usuarioService.obtenerPorId(id);
+    }
+
+    @GetMapping("/query")
+    public ArrayList<UsuarioModel> obtenerUsuarioPorPrioridad(@RequestParam("prioridad") Integer prioridad){
+        return this.usuarioService.obtenerPorPrioridad(prioridad);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public String eliminarPorId(@PathVariable("id") Long id){
+        boolean ok = this.usuarioService.eliminarUsuario(id);
+        return ok ? "Se eliminó el usario con id "+id :"No pudo eliminar el usuario con id "+id;
+    }
+}
